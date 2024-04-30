850f85c8ca261c3fc35f67b5a16a4a79ae5ed3ec
masfazan <msfazan@icloud.com>
2024-04-30 14:09:15 -0300

    correção

--- EstanteDezLivros/Program.cs
+++ EstanteDezLivros/Program.cs
@@ -3,6 +3,15 @@ char resposta;
 Livro livro = new Livro();
 Livro[] livros = new Livro[10];
 
+int Menu()
+{
+    Console.WriteLine("1-Cadastrar livro");
+    Console.WriteLine("2-Imprimir estante");
+    Console.WriteLine("3-Imprimir livro específico");
+    Console.WriteLine("4-Sair do cadastro");
+    Console.WriteLine("Escolha sua opção: ");
+    return int.Parse(Console.ReadLine());
+}
 
 Livro InserirLivro()
 {
@@ -17,51 +26,65 @@ Livro InserirLivro()
 
     return livroInserido;
 }
+void AdicionarLivroEstante()
+{
+    do
+    {
+        Console.WriteLine($"Cadastre o Livro {cadastro + 1}");
+        livros[cadastro] = InserirLivro();
+        Console.WriteLine();
+        cadastro++;
+        if (cadastro >= 2)
+        {
+            Console.WriteLine("Estante cheia!");
+            break;
+        }
+        else
+        {
+            Console.WriteLine("Gostaria de incluir mais um?\nDigite: S-para sim e N-para não");
+            resposta = char.Parse(Console.ReadLine());
+        }
+        if (resposta == 'n') ;
+        break;
+
+    } while (resposta == 's');
+}
+void ImprimirEstante()
+{
+    for (int i = 0; i < cadastro; i++)
+{
+        Console.WriteLine($"Livro {i + 1}:");
+        livros[i].ImprimirLivro();
+    }
+}
+void BuscarLivro(int i)
+{
+    livros[i].ImprimirLivro();
+}
 
 do
 {
-    Console.WriteLine($"Cadastre o Livro {cadastro + 1}");
-    livros[cadastro] = InserirLivro();
-    Console.WriteLine();
-    cadastro++;
-    if (cadastro >= 2)
+    switch (Menu() )
     {
-        Console.WriteLine("Estante cheia!");
-        break;
+        case 1:
+            AdicionarLivroEstante();
+            break;
+        case 2:
+            ImprimirEstante();
+            break;
+        case 3:
+            Console.WriteLine("Informe o índice do livro: ");
+            BuscarLivro(int.Parse(Console.ReadLine()));
+            break;
+        case 4:
+            Environment.Exit(0);
+            break;
+        default:
+            Console.WriteLine("Opção inválida!");
+            break;
+
     }
-    else
-    {
-        Console.WriteLine("Gostaria de incluir mais um?\nDigite: S-para sim e N-para não");
-        resposta = char.Parse(Console.ReadLine());
-    }
-    if (resposta == 'n') ;
-    break;
-
-} while (resposta == 's');
-
-Console.ReadLine();
-
-for (int i = 0; i < 2; i++)
-{
-    Console.WriteLine($"Livro {i + 1}:");
-    livros[i].ImprimirLivro();//metodo não instanciado
-}
+}while(true);
 Console.ReadLine();
 
 
-
-
-
-
-/*for (int i = 0; i < 1; i++)
-{
-   
-    Console.WriteLine($"Cadastre o Livro {i+1}");
-    livros[i] = InserirLivro();
-    Console.WriteLine();
-}
-for (int i = 0; i < 2; i++)
-{
-    Console.WriteLine($"Livro {i + 1}:");
-    livros[i].ImprimirLivro();
-}*/
