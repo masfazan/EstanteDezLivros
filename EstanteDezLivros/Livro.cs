cf4050b3e17d78d5602581665a422d0b753d591b
masfazan <msfazan@icloud.com>
2024-04-29 21:17:05 -0300

    Adicionar arquivos de projeto.

---
+++ EstanteDezLivros/Livro.cs
@@ -0,0 +1,57 @@
+﻿
+public class Livro
+{
+    public string Titulo;
+    public string Autor;
+    public string Editora;
+    public string Edicao;
+    public int ISBN;
+    public int Paginas;
+
+    public Livro()
+    {
+
+    }
+    public string DefinirTitulo()
+    {
+        Console.WriteLine("Qual o título do livro? ");
+        return Console.ReadLine();
+    }
+
+    public string DefinirAutor()
+    {
+        Console.WriteLine("Qual autor ou quais autores do livro? ");
+        return Console.ReadLine();
+    }
+    public string DefinirEditora()
+    {
+        Console.WriteLine("Qual a editora do livro? ");
+        return Console.ReadLine();
+    }
+    public string DefinirEdicao()
+    {
+        Console.WriteLine("Qual a edição do livro? ");
+        return Console.ReadLine();
+    }
+    public int DefinirIsbn()
+    {
+        Console.WriteLine("Qual o ISBN do livro? ");
+        return int.Parse(Console.ReadLine());
+    }
+    public int DefinirPagina()
+    {
+        Console.WriteLine("Quantas páginas possui o livro? ");
+        return int.Parse(Console.ReadLine());
+    }
+
+    public void ImprimirLivro()
+    {
+        Console.WriteLine($"Título{Titulo}", this.Titulo);
+        Console.WriteLine($"Autor(es){Autor}", this.Autor);
+        Console.WriteLine($"Editora{Editora}", this.Editora);
+        Console.WriteLine($"Edição{Edicao}", this.Edicao);
+        Console.WriteLine($"ISBN{ISBN}", this.ISBN);
+        Console.WriteLine($"Páginas{Paginas}", this.Paginas);
+    }
+
+}
---
+++ EstanteDezLivros/Program.cs
@@ -0,0 +1,30 @@
+﻿int[] livros = int[10];
+Livro livro = new Livro();
+
+
+Livro InserirLivro()
+{
+    Livro livroInserido= new Livro();
+
+    livroInserido.Titulo = livroInserido.DefinirTitulo();
+    livroInserido.Autor = livroInserido.DefinirAutor();
+    livroInserido.Editora= livroInserido.DefinirEditora();
+    livroInserido.Edicao= livroInserido.DefinirEdicao();
+    livroInserido.ISBN = livroInserido.DefinirIsbn();
+    livroInserido.Paginas= livroInserido.DefinirPagina();
+
+    return livroInserido;
+}
+
+for (int i = 0; i < 2; i++)
+{
+    Console.WriteLine($"Cadastre o {livros[i]}");
+    livro =InserirLivro();
+    //Console.ReadLine(livros[i]);
+}
+for (int i = 0; i < 2; i++)
+{
+    Console.WriteLine($"Livro{livros[i]}");
+}
+
+
